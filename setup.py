2906365a9e15a825468f452a89a07c9bb8372772
x6doooo <x6doooo@gmail.com>
2017-06-07 00:11:55 +0800

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['roda'],
     package_dir={'': 'src'},
     include_package_data=True,
-    install_requires=['nose', 'talib', 'numpy'],
+    install_requires=['nose', 'talib', 'numpy', 'pymongo'],
     author='x6doooo',
     author_email='x6doooo@gmail.com',
     description='...',
